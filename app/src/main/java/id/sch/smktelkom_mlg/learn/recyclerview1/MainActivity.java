835b80dc81796282282f8fb94ad4a364ccd49147
Dhita Shandy Faristyawati <dhitashandy12@gmail.com>
2016-11-03 17:53:04 +0700

    RecyclerView List

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/MainActivity.java
@@ -47,5 +47,6 @@ public class MainActivity extends AppCompatActivity {
             mList.add(new Hotel(arJudul[i], arDeskripsi[i], arFoto[i]));
         }
         mAdapter.notifyDataSetChanged();
+
     }
 }
